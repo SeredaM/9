36fd735ab0a5ddeea3fc1cbaae8ed67ca9f85589
SeredaM <75209816+SeredaM@users.noreply.github.com>
2020-12-06 15:37:06 +0300

    Add files via upload

--- 9.6/main.c
+++ 9.6/main.c
@@ -1,66 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-int nod(int a, int b)
+void change(int i,float *w, float *m,int h)
 {
-    a=abs(a);
-    b=abs(b);
-if ((a==0)||(b==0)) return 1;
-while (a!=b)
+    int k=i+1;
+    float t;
+    while (m[k*h+i]==0) k++;
+    if (k<h-1)
+    for (int j=0; j<h; j++)
+    {
+      t=m[h*i+j];
+      m[h*i+j]=m[k*h+j];
+      m[k*h+j]=m[h*i+j];
+      t=w[h*i+j];
+      w[h*i+j]=w[k*h+j];
+      w[k*h+j]=w[h*i+j];
+    }
+}
+void backw(float *m, float *w, int h)
 {
-if (a>b)
-{
-a=a-b;
-}
-else
-{
-b=b-a;
-}
-}
-return a;
-}
-void backw(int *m, int *w, int h)
-{ int d;
+    float d;
     for (int i=h-1;i>=1;i--)
         for (int j=i-1;j>=0;j--)
         {
-        d=nod(m[h*j+i],m[h*i+i]);
+        d=m[h*j+i]/m[h*i+i];
           for(int k=0;k<h;k++)
-            w[h*j+k]=(w[h*j+k]*m[h*i+i] - w[h*i+k]*m[h*j+i])/d;
+            w[h*j+k]=(w[h*j+k] - w[h*i+k]*d);
         m[h*j+i]=0;
         }
 }
-void forw(int *m, int *w, int h)
+void forw(float *m, float *w, int h)
 {
-    int a,d;
+    float a,d;
      for (int i=0;i<h-1;i++)
         for (int j=i+1;j<h;j++)
         {
+         if (abs(m[h*i+i])<0.0001) change(i,w,m,h);
          a=m[h*j+i];
-         d=nod(a,m[h*i+i]);
+         d=m[h*j+i]/m[h*i+i];
          if (a!=0)
-            for (int k=i;k<h;k++)
+            for (int k=0;k<h;k++)
         {
-            w[h*j+k] =  (w[h*j+k]*m[h*i+i] - a*w[h*i+k])/d;
-            m[h*j+k]= ((m[h*j+k]*m[h*i+i]) - (a*m[h*i+k]))/d;
+            w[h*j+k] = w[h*j+k] - d*w[h*i+k];
+            m[h*j+k]= (m[h*j+k]) - d*m[h*i+k];
         }
         }
 }
-void gauss(int *m, int *w, int h)
+void gauss(float *m, float *w, int h)
 {
     forw(m,w,h);
     backw(m,w,h);
+    for (int i=0;i<h;i++)
+    {
+     for (int j=0;j<h;j++)
+        w[i*h+j]=w[i*h+j]/m[i*h+i];
+     m[i*h+i]=1;
+    }
+
+
 
 }
 
 int main()
 {
-   int *m,*w,h;
+   float *m,*w;
+   int h;
    printf("Enter size: \n");
    scanf("%d",&h);
    printf("\nEnter matrix: \n");
-  m = malloc(sizeof(int)*h*h);
-  w = malloc(sizeof(int)*h*h);
+  m = malloc(sizeof(float)*h*h);
+  w = malloc(sizeof(float)*h*h);
    for (int i=0;i<h;i++)
     for (int j=0;j<h;j++)
     {
@@ -70,7 +79,7 @@ int main()
      }
      else
      w[i*h+j] = 0;
-     scanf("%d",&m[i*h+j]);
+     scanf("%f",&m[i*h+j]);
     }
    gauss(m,w,h);
    printf("Inverse matrix:\n");
@@ -78,9 +87,7 @@ int main()
     {
     printf("\n");
     for (int j=0;j<h;j++)
-        if (w[i*h+j]==0) printf("0 ");
-         else if (m[i*h+i]==1) printf("%d ",w[i*h+j]);
-           else printf("%d/%d ",w[i*h+j],m[i*h+i]);
+        printf("%.3f ",w[i*h+j]);
     }
     printf("\nCheck:\n");
     gauss(w,m,h);
@@ -88,9 +95,7 @@ int main()
     {
     printf("\n");
     for (int j=0;j<h;j++)
-        if (m[i*h+j]==0) printf("0 ");
-         else if (w[i*h+i]==1) printf("%d ",m[i*h+j]);
-           else printf("%d/%d ",m[i*h+j],w[i*h+i]);
+        printf("%.3f ",m[i*h+j]);
     }
     return 0;
 }
